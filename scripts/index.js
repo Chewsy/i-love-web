095ae394d0f4d668c46b63040f7e15b7f026d452
Chewsy <13772434+Chewsy@users.noreply.github.com>
2026-02-01 23:57:29 +0100

    Styling class toggle op folders

--- scripts/index.js
+++ scripts/index.js
@@ -3,6 +3,9 @@ let closeButton = document.querySelector("#closeButton");
 
 function close() {
     document.querySelector("#frame").style.display = 'none';
+
+    let target = document.querySelector(".selectedGrid");
+    target.classList.remove("selectedGrid");
 }
 
 closeButton.addEventListener("click", close);
@@ -10,10 +13,11 @@ closeButton.addEventListener("click", close);
 
 let learningJournalButton = document.querySelector("#learningJournal");
 
-// TODO navragen docent meegeven parameter met target id ipv elke button een functie geven
-function openFrame(){
-        document.querySelector("#frame").style.display = 'block';
+function openFrame(event) {
+    document.querySelector("#frame").style.display = 'block';
+
+    let target = event.target.closest("div");
+    target.classList.add("selectedGrid")
 }
 
 learningJournalButton.addEventListener("click", openFrame)
-
