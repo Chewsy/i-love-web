e4584958423680426e08592e4f22176cbf31ebea
Chewsy <13772434+Chewsy@users.noreply.github.com>
2026-09-06 22:10:38 +0200

    ✨feat: initial index page

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,28 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation</p>
+<main>
+    <h1>Learning journal</h1>
+    <p>
+        Lorem ipsum dolor sit amet. Vel optio temporein molestiae rem velit
+        perferendis. Ut sint alias <a
+            href="https://www.loremipzum.com"
+            target="_blank"
+            >Ex ratione non consequuntur similique est itaque expedita est dolor
+            error</a
+        > aut architecto dolore ut officia aspernatur. Ea recusandae labore ex galisum
+        nobisEst illo ad galisum sint.
+    </p>
+    <p>
+        Eos deleniti temporibus est autem ipsumsed Quis et temporibus similique.
+        Aut dolor quia a officiis commodiest quod. Et perferendis quibusdam et
+        veniam blanditiisqui molestiae.
+    </p>
+    <p>
+        Ut rerum voluptatem <a href="https://www.loremipzum.com" target="_blank"
+            >Id tenetur et ipsa esse ad nemo reiciendis ut esse accusamus</a
+        > qui eaque mollitia et esse amet. Sed dolorem modi sed aliquid ullamNon
+        exercitationem et pariatur quas nam sapiente molestiae. Aut sint nisi cum
+        expedita necessitatibusEst iure ut voluptatum fugiat et dignissimos laudantium
+        ab pariatur quia qui ducimus debitis. Nam consequatur repudiandaeQuo explicabo
+        At aspernatur tempora et voluptatem nihil ea dolores voluptatem aut quidem
+        fugit.
+    </p>
+</main>
